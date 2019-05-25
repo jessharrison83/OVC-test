871fd283bcdf6365cf9855ae874a38f4907ccc64
Jessica <jess@Jessicas-iMac.local>
2019-05-25 17:36:39 -0400

    styling
    background color changed for even rows on table, hover property added to select rows, box shadow added for depth.

--- ovc-app/src/styles/index.js
+++ ovc-app/src/styles/index.js
@@ -1,24 +1,37 @@
 import styled from 'styled-components';
 
 export const Tbl = styled.table `
-  font-family: arial, sans-serif;
+  @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
+  font-family: 'Montserrat', sans-serif;
   border-collapse: collapse;
   width: 100%;
   margin-top: 20%;
+  box-shadow: 3px 5px 10px #EDEEEF;
+  
+  
 `
 export const Heading = styled.th `
-  border: 1px solid #dddddd;
+  border: 1px solid #ECF3F9;
   text-align: left;
   padding: 8px;
 `
 export const Data = styled.td `
-  border: 1px solid #dddddd;
+  border: 1px solid #ECF3F9;
   text-align: left;
   padding: 8px;
 `
 
 export const Row = styled.tr `
   &:nth-child(even) {
-    background-color: #dddddd;
+    background-color: #ECF3F9;
+    box-shadow: 1px 2px 2px #EDEEEF;
   }
+  &:hover {
+    &:not(:first-child){
+    color: #ECF3F9;
+    background-color: #43627D;
+    cursor: pointer;
+  }
+  }
+
 `
