32f3288a17b33325612080a4b25347888962007d
Jessica <jess@Jessicas-iMac.local>
2019-05-25 16:42:12 -0400

    data fetching
    component is fetching data from endpoint as expected, skeleton of app is complete and working.

--- ovc-app/src/components/Table.js
+++ ovc-app/src/components/Table.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+import { fetchUsers } from '../actions';
 
 class Table extends Component {
+
+componentDidMount() {
+  this.props.fetchUsers();
+}
+
   render() {
     return (
-      <h1>is everything rendering?</h1>
-    )
+      this.props.users.map(user => {
+        return (<h1>{user.name}</h1>)
+})
+)
   }   
 }
 
+const mapStateToProps = state => {
+  return {
+    users: state.tableReducer.users,
+    loading: state.tableReducer.loading,
+    error: state.tableReducer.error
+  }
+}
 
-export default Table
+export default connect(
+  mapStateToProps, 
+  { fetchUsers }
+)(Table)
