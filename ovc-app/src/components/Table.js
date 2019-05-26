9f799a9020578da21ca0a1af61934d4fd79bbf3d
Jess <jessdev83@gmail.com>
2019-05-25 20:48:25 -0400

    formatting

--- ovc-app/src/components/Table.js
+++ ovc-app/src/components/Table.js
@@ -1,50 +1,43 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 import { Tbl, Heading, Data, Row, Loader } from '../styles';
 import { fetchUsers } from '../actions';
 
 class Table extends Component {
-
-componentDidMount() {
-  this.props.fetchUsers();
-}
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
 
   render() {
     if (this.props.loading) {
-      return (
-        <Loader>Fetching users... </Loader>
-      )
+      return <Loader>Fetching users... </Loader>;
     } else if (this.props.error) {
-      return (
-        <Loader>Oops! Cannot find users. :(</Loader>
-      )
+      return <Loader>Oops! Cannot find users. :(</Loader>;
     } else {
       return (
         <Tbl>
           <tbody>
-          <Row>
-            <Heading data-testid="name">Name</Heading>
-            <Heading>Email</Heading>
-            <Heading>City</Heading>
-            <Heading>Company</Heading>
-          </Row>
-          {this.props.users.map(user => {
-          return (
-          <Row className="dataRows" key={user.id}>
-            <Data>{user.name}</Data>
-            <Data>{user.email}</Data>
-            <Data>{user.address.city}</Data>
-            <Data>{user.company.name}</Data>
-          </Row>
-          )
-        })}
+            <Row>
+              <Heading data-testid="name">Name</Heading>
+              <Heading>Email</Heading>
+              <Heading>City</Heading>
+              <Heading>Company</Heading>
+            </Row>
+            {this.props.users.map(user => {
+              return (
+                <Row className="dataRows" key={user.id}>
+                  <Data>{user.name}</Data>
+                  <Data>{user.email}</Data>
+                  <Data>{user.address.city}</Data>
+                  <Data>{user.company.name}</Data>
+                </Row>
+              );
+            })}
           </tbody>
         </Tbl>
-  )
+      );
     }
-
-    
-  }   
+  }
 }
 
 const mapStateToProps = state => {
@@ -52,10 +45,10 @@ const mapStateToProps = state => {
     users: state.tableReducer.users,
     loading: state.tableReducer.loading,
     error: state.tableReducer.error
-  }
-}
+  };
+};
 
 export default connect(
-  mapStateToProps, 
+  mapStateToProps,
   { fetchUsers }
-)(Table)
+)(Table);
